Fix normalization in training workflow

src/resoterre/experiments/rdps_to_hrdps_training.py
@@ -107,9 +107,11 @@ class RDPSToHRDPSTrainingFromConfig:
         self.optimizer = optim.Adam(self.unet.parameters(), lr=config.learning_rate, weight_decay=config.weight_decay)
         self.losses: list[float] = []
         self.minima_tracker = MinimaTracker(minimum_metrics_to_track=["(Loss)", "Loss", "ValidationLoss"])
+        self.minimum_validation_loss_model: Path | None = None
         self.total_iterations = 0
+        self.mse_loss = nn.MSELoss()
 
-    def save_state(self, epoch: int, include_safetensors: bool = False) -> None:
+    def save_state(self, epoch: int, include_safetensors: bool = False, new_validation_minimum: bool = False) -> None:
         """
         Save the current state of the UNet model, optimizer, and training progress.
 
@@ -119,17 +121,22 @@ class RDPSToHRDPSTrainingFromConfig:
             Current epoch number.
         include_safetensors : bool
             Whether to save the model state in safetensors format.
+        new_validation_minimum : bool
+            Whether the current epoch has achieved a new minimum validation loss.
         """
         if self.config.path_output is None:
             raise ValueError("Output path is not specified in the configuration.")
         experiment_name = "skeleton"
         output_path = Path(self.config.path_output, f"{experiment_name}_unet_epoch_{epoch:03d}.pth")
+        if new_validation_minimum:
+            self.minimum_validation_loss_model = output_path
         torch.save(
             {
                 "model_state_dict": self.unet.state_dict(),
                 "optimizer_state_dict": self.optimizer.state_dict(),
                 "total_iterations": self.total_iterations,
                 "minima_tracker": self.minima_tracker,
+                "minimum_validation_loss_model": str(self.minimum_validation_loss_model),
             },
             output_path,
         )
@@ -169,6 +176,83 @@ class RDPSToHRDPSTrainingFromConfig:
         self.minima_tracker = checkpoint.get(
             "minima_tracker", MinimaTracker(minimum_metrics_to_track=["(Loss)", "Loss", "ValidationLoss"])
         )
+        self.minimum_validation_loss_model = checkpoint.get("minimum_validation_loss_model", str(input_path))
+
+    def to_device(self, device: str) -> None:
+        """
+        Move the UNet model and optimizer to the specified device.
+
+        Parameters
+        ----------
+        device : str
+            Device to move the model and optimizer to (e.g., 'cpu', 'cuda').
+        """
+        self.unet.to(device)
+        for state in self.optimizer.state.values():
+            for k, v in state.items():
+                if isinstance(v, torch.Tensor):
+                    state[k] = v.to(device)
+
+    def training_step(self, item: dict[str, torch.Tensor], epoch: int) -> None:
+        """
+        Training step for a single batch of data.
+
+        Parameters
+        ----------
+        item : dict[str, torch.Tensor]
+            Dictionary containing input and target tensors for the batch.
+        epoch : int
+            Current epoch number.
+        """
+        if self.config.path_output is None:
+            raise ValueError("Output path is not specified in the configuration.")
+        self.optimizer.zero_grad()
+        input_data = item["input_first_block"].to(self.config.device, non_blocking=True)
+        target_data = item["target"].to(self.config.device, non_blocking=True)
+        if "input_last_layer" in item:
+            input_last_layer = item["input_last_layer"].to(self.config.device, non_blocking=True)
+        else:
+            input_last_layer = None
+        output = self.unet(input_data, x_last_layer=input_last_layer)
+        loss_terms = {}
+        weights = []
+        mse_weight = 1.0
+        if mse_weight > 0:
+            loss_terms["mse_loss"] = self.mse_loss(output, target_data)
+            weights.append(mse_weight)
+        # if ssim_weight > 0:
+        #     loss_terms["ssim_loss"] = 1 - ssim_loss(output, target_data)
+        #     weights.append(config.ssim_weight)
+        loss = sum(loss_terms[term] * weight for term, weight in zip(loss_terms.keys(), weights, strict=True))
+        loss.backward()
+        self.optimizer.step()
+        self.losses.append(loss.item())
+        self.total_iterations += 1
+        new_minima = self.minima_tracker.update_minima(
+            iteration=len(self.losses),
+            metrics_values={"Loss": self.losses[-1]},
+            epoch=epoch,
+            return_true_for=["Loss"],
+        )
+        if new_minima:
+            iteration_str = f"{self.total_iterations:7d} (epoch {epoch:2d})"
+            logger.info("Iteration %s, loss: %s (new minimum)", iteration_str, readable_value(self.losses[-1]))
+
+        if self.config.device == "cuda":
+            output = output.detach().cpu().numpy()
+            input_dict = {"input": input_data.detach().cpu().numpy()}
+            target_data = target_data.detach().cpu().numpy()
+            if input_last_layer is not None:
+                input_dict["input_last_layer"] = input_last_layer.detach().cpu().numpy()
+        ml_sample_figures(
+            path_output=Path(self.config.path_output),
+            count=self.total_iterations,
+            input_data=input_dict,
+            target_data=target_data,
+            output_data=output,
+            output_labels=self.config.hrdps_variables_for_training,
+            scheduler=ActionScheduler(every_progression=self.config.debug_training_figures_progression),
+        )
 
     def __call__(self, epoch: int) -> None:
         """
@@ -179,63 +263,14 @@ class RDPSToHRDPSTrainingFromConfig:
         epoch : int
             Current epoch number.
         """
-        if self.config.path_output is None:
-            raise ValueError("Output path is not specified in the configuration.")
         if epoch > 1 and not self.losses:
             self.load_state(epoch=epoch - 1)
         torch.set_num_threads(self.config.num_threads)
-        self.unet.to(self.config.device)
-        for state in self.optimizer.state.values():
-            for k, v in state.items():
-                if isinstance(v, torch.Tensor):
-                    state[k] = v.to(self.config.device)
+        self.to_device(self.config.device)
         self.unet.train()
-        mse_loss = nn.MSELoss()
+        self.mse_loss = nn.MSELoss()
         for item in self.data_loader:
-            self.optimizer.zero_grad()
-            input_data = item["input_first_block"].to(self.config.device, non_blocking=True)
-            target_data = item["target"].to(self.config.device, non_blocking=True)
-            if "input_last_layer" in item:
-                input_last_layer = item["input_last_layer"].to(self.config.device, non_blocking=True)
-            else:
-                input_last_layer = None
-            output = self.unet(input_data, x_last_layer=input_last_layer)
-            loss_terms = {}
-            weights = []
-            mse_weight = 1.0
-            if mse_weight > 0:
-                loss_terms["mse_loss"] = mse_loss(output, target_data)
-                weights.append(mse_weight)
-            # if ssim_weight > 0:
-            #     loss_terms["ssim_loss"] = 1 - ssim_loss(output, target_data)
-            #     weights.append(config.ssim_weight)
-            loss = sum(loss_terms[term] * weight for term, weight in zip(loss_terms.keys(), weights, strict=True))
-            loss.backward()
-            self.optimizer.step()
-            self.losses.append(loss.item())
-            self.total_iterations += 1
-            new_minima = self.minima_tracker.update_minima(
-                iteration=len(self.losses),
-                metrics_values={"Loss": self.losses[-1]},
-                epoch=epoch,
-                return_true_for=["Loss"],
-            )
-            if new_minima:
-                iteration_str = f"{self.total_iterations:7d} (epoch {epoch:2d})"
-                logger.info("Iteration %s, loss: %s (new minimum)", iteration_str, readable_value(self.losses[-1]))
-
-            if self.config.device == "cuda":
-                output = output.detach().cpu()
-                input_data = input_data.detach().cpu()
-                target_data = target_data.detach().cpu()
-            ml_sample_figures(
-                path_output=Path(self.config.path_output),
-                count=self.total_iterations,
-                input_data=input_data,
-                target_data=target_data,
-                output_data=output,
-                scheduler=ActionScheduler(every_progression=self.config.debug_training_figures_progression),
-            )
+            self.training_step(item, epoch=epoch)
         if self.config.validation_period_start:
             self.unet.train(False)
             self.dataset.active_split_name = "validation"
@@ -258,7 +293,7 @@ class RDPSToHRDPSTrainingFromConfig:
                     else:
                         input_last_layer = None
                     output = self.unet(input_data, x_last_layer=input_last_layer)
-                    validation_losses.append(mse_loss(output, target_data).item())
+                    validation_losses.append(self.mse_loss(output, target_data).item())
                 validation_loss = sum(validation_losses) / len(validation_losses)
                 new_minima = self.minima_tracker.update_minima(
                     iteration=len(self.losses),
@@ -272,4 +307,4 @@ class RDPSToHRDPSTrainingFromConfig:
                 logger.info(
                     "End of epoch %2d, validation loss: %s%s", epoch, readable_value(validation_loss), minimum_str
                 )
-        self.save_state(epoch=epoch, include_safetensors=True)
+        self.save_state(epoch=epoch, include_safetensors=True, new_validation_minimum=new_minima)

src/resoterre/hybrid_data_loaders/rdps_to_hrdps.py
@@ -12,7 +12,9 @@ from torch.utils.data import DataLoader, Dataset
 
 from resoterre.config_utils import register_config
 from resoterre.data_management.timeseries import overlapping_datetimes_indices
-from resoterre.datasets.hrdps.hrdps_variables import hrdps_variables, long_variable_name, short_variable_name
+from resoterre.datasets.hrdps.hrdps_variables import hrdps_variables as hrdps_variables_collection
+from resoterre.datasets.hrdps.hrdps_variables import long_variable_name, short_variable_name
+from resoterre.datasets.rdps.rdps_variables import rdps_variables as rdps_variables_collection
 from resoterre.ml.data_loader_utils import (
     DatasetConfig,
     DatasetFromNetCDFSave,
@@ -149,11 +151,11 @@ def post_process_model_output(
             add_climatology = True
         else:
             add_climatology = False
-        normalize_min_local = hrdps_variables[variable_name].normalize_min
+        normalize_min_local = hrdps_variables_collection[variable_name].normalize_min
         if normalize_min_local is None:
             raise ValueError(f"Variable {variable_name} does not have normalization parameters defined.")
         normalize_min: float = normalize_min_local
-        normalize_max_local = hrdps_variables[variable_name].normalize_max
+        normalize_max_local = hrdps_variables_collection[variable_name].normalize_max
         if normalize_max_local is None:
             raise ValueError(f"Variable {variable_name} does not have normalization parameters defined.")
         normalize_max: float = normalize_max_local
@@ -168,8 +170,8 @@ def post_process_model_output(
             known_min=normalize_min,
             known_max=normalize_max,
             mode=(-1, 1),
-            log_normalize=hrdps_variables[variable_name].log_normalize,
-            log_offset=hrdps_variables[variable_name].normalize_log_offset,
+            log_normalize=hrdps_variables_collection[variable_name].log_normalize,
+            log_offset=hrdps_variables_collection[variable_name].normalize_log_offset,
         )
         debug_plots(
             plot_data=output_variables[variable_name][0, 0, :, :],
@@ -300,13 +302,15 @@ class RDPSToHRDPSZarrDataset(Dataset):  # type: ignore[misc]
         List of RDPS variables to include in the dataset.
     hrdps_variables : list[str]
         List of HRDPS variables to include in the dataset.
-    geophysical_variables : list[str] | None
+    geophysical_variables : list[str], optional
         List of geophysical variables to include in the dataset.
-    validation_period_start : datetime.datetime
+    validation_period_start : datetime.datetime, optional
         Start datetime for the validation period.
-    test_period_start : datetime.datetime
+    test_period_start : datetime.datetime, optional
         Start datetime for the test period.
-    debug_num_samples : int
+    test_period_end : datetime.datetime, optional
+        End datetime for the test period. If None, the end of the dataset is used.
+    debug_num_samples : int, optional
         Number of sample in the dataset for debugging purposes. If None, all samples are used.
     """
 
@@ -319,6 +323,7 @@ class RDPSToHRDPSZarrDataset(Dataset):  # type: ignore[misc]
         geophysical_variables: list[str] | None = None,
         validation_period_start: datetime.datetime | None = None,
         test_period_start: datetime.datetime | None = None,
+        test_period_end: datetime.datetime | None = None,
         debug_num_samples: int | None = None,
     ) -> None:
         self.path_zarr_rdps = path_zarr_rdps
@@ -363,6 +368,8 @@ class RDPSToHRDPSZarrDataset(Dataset):  # type: ignore[misc]
         if (validation_period_start is not None) or (test_period_start is not None):
             validation_start_idx = 0
             validation_end_idx = len(self.valid_time_idx)
+            test_start_idx = 0
+            test_end_idx = len(self.valid_time_idx)
             for i, time_idx in enumerate(self.valid_time_idx):
                 current_datetime = rdps_times[time_idx + self.rdps_start_idx]
                 current_datetime = current_datetime.astype("datetime64[us]").astype(object)
@@ -377,8 +384,17 @@ class RDPSToHRDPSZarrDataset(Dataset):  # type: ignore[misc]
                     if self.train_time_idx is None:
                         self.train_time_idx = self.valid_time_idx[:i]
                     validation_end_idx = i
+                    test_start_idx = i
                     self.test_time_idx = self.valid_time_idx[i:]
                     break
+            if test_period_end is not None:
+                for j, time_idx in enumerate(self.valid_time_idx[test_start_idx:]):
+                    current_datetime = rdps_times[time_idx + self.rdps_start_idx]
+                    current_datetime = current_datetime.astype("datetime64[us]").astype(object)
+                    if current_datetime > test_period_end:
+                        test_end_idx = j + test_start_idx
+                        break
+                self.test_time_idx = self.valid_time_idx[test_start_idx:test_end_idx]
             if validation_period_start is not None:
                 self.validation_time_idx = self.valid_time_idx[validation_start_idx:validation_end_idx]
 
@@ -398,7 +414,7 @@ class RDPSToHRDPSZarrDataset(Dataset):  # type: ignore[misc]
     @property
     def split_valid_time_idx(self) -> list[int]:
         """
-        Return the valid time indices for the active split.
+        The valid time indices for the active split.
 
         Returns
         -------
@@ -436,7 +452,13 @@ class RDPSToHRDPSZarrDataset(Dataset):  # type: ignore[misc]
             dtype=np.float32,
         )
         for i, variable_name in enumerate(self.geophysical_variables):
-            input_last_layer[i, :, :] = self.ds_hrdps[variable_name].values
+            input_last_layer[i, :, :] = normalize(
+                self.ds_hrdps[variable_name].values,
+                valid_min=hrdps_variables_collection[variable_name].normalize_min,
+                valid_max=hrdps_variables_collection[variable_name].normalize_max,
+                log_normalize=hrdps_variables_collection[variable_name].log_normalize,
+                log_offset=hrdps_variables_collection[variable_name].normalize_log_offset,
+            )
         return {"input_last_layer": input_last_layer}
 
     def __getitem__(self, idx: int) -> dict[str, np.ndarray]:
@@ -462,8 +484,15 @@ class RDPSToHRDPSZarrDataset(Dataset):  # type: ignore[misc]
         )
         for i, rdps_variable in enumerate(self.rdps_variables):
             rdps_data = self.ds_rdps.isel(time=rdps_idx)[rdps_variable].values
+            # ToDo: some data validation failed and there are still negative precipitations in RDPS
+            if rdps_variables_collection[rdps_variable].log_normalize:
+                rdps_data = np.clip(rdps_data, a_min=0.0, a_max=None)
             input_first_block[i, :, :] = normalize(
-                rdps_data, valid_min=-40.0, valid_max=40.0, log_normalize=False, log_offset=1e-12
+                rdps_data,
+                valid_min=rdps_variables_collection[rdps_variable].normalize_min,
+                valid_max=rdps_variables_collection[rdps_variable].normalize_max,
+                log_normalize=rdps_variables_collection[rdps_variable].log_normalize,
+                log_offset=rdps_variables_collection[rdps_variable].normalize_log_offset,
             )
         if self.ds_hrdps is None:
             self.ds_hrdps = xarray.open_zarr(self.path_zarr_hrdps)
@@ -473,7 +502,11 @@ class RDPSToHRDPSZarrDataset(Dataset):  # type: ignore[misc]
         for i, hrdps_variable in enumerate(self.hrdps_variables):
             hrdps_data = self.ds_hrdps.isel(time=hrdps_idx)[hrdps_variable].values
             target[i, :, :] = normalize(
-                hrdps_data, valid_min=-40.0, valid_max=40.0, log_normalize=False, log_offset=1e-12
+                hrdps_data,
+                valid_min=hrdps_variables_collection[hrdps_variable].normalize_min,
+                valid_max=hrdps_variables_collection[hrdps_variable].normalize_max,
+                log_normalize=hrdps_variables_collection[hrdps_variable].log_normalize,
+                log_offset=hrdps_variables_collection[hrdps_variable].normalize_log_offset,
             )
         current_datetime = self.ds_rdps["time"].values[rdps_idx]
         current_datetime = current_datetime.astype("datetime64[us]").astype(object)