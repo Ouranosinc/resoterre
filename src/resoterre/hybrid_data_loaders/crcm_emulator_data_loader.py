712d33a0f76cf6d734876ca5d00c009edbda1071
Blaise Gauvin St-Denis <bstdenis@gmail.com>
2026-09-10 11:23:19 -0400

    Add option to retrieve variables with vertical levels as 3d fields in CRCM emulator dataset

--- src/resoterre/hybrid_data_loaders/crcm_emulator_data_loader.py
+++ src/resoterre/hybrid_data_loaders/crcm_emulator_data_loader.py
@@ -36,6 +36,8 @@ class CRCMEmulatorDataset(td.Dataset):  # type: ignore[misc]
         List of CRCM variable names.
     time_periods : list[Any]
         List of time periods to consider. Each time period is a tuple of start and end times.
+    keep_3d_variables : bool
+        Whether to keep 3D variables structure in the dataset.
     max_open_dataset : int
         Maximum number of open xarray datasets to cache.
     """
@@ -48,6 +50,7 @@ class CRCMEmulatorDataset(td.Dataset):  # type: ignore[misc]
         gcm_variables: list[str],
         crcm_variables: list[str],
         time_periods: list[Any],
+        keep_3d_variables: bool = False,
         max_open_dataset: int = 2,
     ) -> None:
         self.path_gcm_preprocessing = path_gcm_preprocessing
@@ -59,6 +62,7 @@ class CRCMEmulatorDataset(td.Dataset):  # type: ignore[misc]
         self.crcm_zarr = {}
         self.gcm_open_dataset: dict[str, xarray.Dataset] = {}
         self.crcm_open_dataset: dict[str, xarray.Dataset] = {}
+        self.keep_3d_variables = keep_3d_variables
         self.max_open_dataset = max_open_dataset
         # ToDo: Only include mask channel for variables that can be under topography at their pressure level
         self.num_input_channels = len(gcm_variables) * 2
@@ -190,23 +194,22 @@ class CRCMEmulatorDataset(td.Dataset):  # type: ignore[misc]
         else:
             raise ValueError(f"Unknown dataset type: {dataset_type}")
 
-    def __getitem__(self, idx: int) -> dict[str, np.ndarray]:
+    def input_first_block_as_2d_channels(self, xarray_dataset_gcm: xarray.Dataset, gcm_idx: int) -> np.ndarray:
         """
-        Get the input and target data for a given index.
+        Get the first block of input data as 2D channels.
 
         Parameters
         ----------
-        idx : int
-            Index of the data to retrieve.
+        xarray_dataset_gcm : xarray.Dataset
+            The open xarray dataset for the GCM data.
+        gcm_idx : int
+            Index of the time step in the GCM dataset.
 
         Returns
         -------
         dict[str, np.ndarray]
-            Dictionary containing the input data, target data, and associated metadata.
+            Dictionary containing the input first block as 2D channels under the key "input_first_block".
         """
-        # ToDo: do mini-batch that come from same file?
-        gcm_str, gcm_idx, crcm_idx = self.valid_idx[idx]
-        xarray_dataset_gcm = self.get_open_dataset("gcm", gcm_str)
         input_first_block = np.zeros(0)  # placeholder
         for i, variable_name in enumerate(self.gcm_variables):
             xarray_variable = xarray_dataset_gcm[variable_name]
@@ -225,6 +228,106 @@ class CRCMEmulatorDataset(td.Dataset):  # type: ignore[misc]
                 log_offset=cmip6_variables[variable_name].normalize_log_offset,
             )
             input_first_block[2 * i + 1, :, :] = gcm_mask.astype(np.float32)
+        return {"input_first_block": input_first_block}
+
+    def input_first_block_as_3d(self, xarray_dataset_gcm: xarray.Dataset, gcm_idx: int) -> dict[str, np.ndarray]:
+        """
+        Get the first block of input data as 3D fields for variables with vertical levels.
+
+        Parameters
+        ----------
+        xarray_dataset_gcm : xarray.Dataset
+            The open xarray dataset for the GCM data.
+        gcm_idx : int
+            Index of the time step in the GCM dataset.
+
+        Returns
+        -------
+        dict[str, np.ndarray]
+            Dictionary containing the input first block as 3D channels.
+        """
+        # Currently expects 3d variables to be ordered in self.gcm_variables
+        # This involves reversing the vertical level separation in the source files, not ideal but acceptable for now.
+        input_first_block = {}
+        vertical_level_counts: dict[str, int] = {}
+        num_2d_channels = 0
+        for variable_name in self.gcm_variables:
+            variable_handle = cmip6_variables[variable_name]
+            key = variable_handle.netcdf_key
+            if key != variable_name:
+                vertical_level_counts[key] = vertical_level_counts.get(key, 0) + 1
+            else:
+                num_2d_channels += 1
+        num_2d_channels += sum([v for v in vertical_level_counts.values() if v == 1])
+        idx_2d = 0
+        for variable_name in self.gcm_variables:
+            variable_handle = cmip6_variables[variable_name]
+            key = variable_handle.netcdf_key
+            xarray_variable = xarray_dataset_gcm[variable_name]
+            input_key = f"input_first_block_{key}"
+            if key in vertical_level_counts and vertical_level_counts[key] > 1:
+                if input_key not in input_first_block:
+                    input_first_block[input_key] = np.zeros(
+                        (vertical_level_counts[key], xarray_variable.shape[1], xarray_variable.shape[2]),
+                        dtype=np.float32,
+                    )
+                    input_first_block[f"input_first_block_{key}_mask"] = np.zeros(
+                        (vertical_level_counts[key], xarray_variable.shape[1], xarray_variable.shape[2]),
+                        dtype=np.float32,
+                    )
+            elif "input_first_block_2d" not in input_first_block:
+                input_first_block["input_first_block_2d"] = np.zeros(
+                    (2 * num_2d_channels, xarray_variable.shape[1], xarray_variable.shape[2]),
+                    dtype=np.float32,
+                )
+            gcm_data = xarray_variable.isel(time=gcm_idx).values
+            gcm_mask = np.isnan(gcm_data)
+            gcm_data[gcm_mask] = 0.0
+            if input_key in input_first_block:
+                input_first_block[input_key][-vertical_level_counts[key], :, :] = normalize(
+                    gcm_data,
+                    valid_min=cmip6_variables[variable_name].normalize_min,
+                    valid_max=cmip6_variables[variable_name].normalize_max,
+                    log_normalize=cmip6_variables[variable_name].log_normalize,
+                    log_offset=cmip6_variables[variable_name].normalize_log_offset,
+                )
+                input_first_block[f"input_first_block_{key}_mask"][-vertical_level_counts[key], :, :] = gcm_mask.astype(
+                    np.float32
+                )
+                vertical_level_counts[key] -= 1
+            else:
+                input_first_block["input_first_block_2d"][idx_2d, :, :] = normalize(
+                    gcm_data,
+                    valid_min=cmip6_variables[variable_name].normalize_min,
+                    valid_max=cmip6_variables[variable_name].normalize_max,
+                    log_normalize=cmip6_variables[variable_name].log_normalize,
+                    log_offset=cmip6_variables[variable_name].normalize_log_offset,
+                )
+                input_first_block["input_first_block_2d"][idx_2d + 1, :, :] = gcm_mask.astype(np.float32)
+                idx_2d += 2
+        return input_first_block
+
+    def __getitem__(self, idx: int) -> dict[str, np.ndarray]:
+        """
+        Get the input and target data for a given index.
+
+        Parameters
+        ----------
+        idx : int
+            Index of the data to retrieve.
+
+        Returns
+        -------
+        dict[str, np.ndarray]
+            Dictionary containing the input data, target data, and associated metadata.
+        """
+        # ToDo: do mini-batch that come from same file?
+        gcm_str, gcm_idx, crcm_idx = self.valid_idx[idx]
+        xarray_dataset_gcm = self.get_open_dataset("gcm", gcm_str)
+        if self.keep_3d_variables:
+            input_first_block = self.input_first_block_as_3d(xarray_dataset_gcm, gcm_idx)
+        else:
+            input_first_block = self.input_first_block_as_2d_channels(xarray_dataset_gcm, gcm_idx)
         emission_data = {
             "CO2": xarray_dataset_gcm["CO2"][gcm_idx].values,
             "CH4": xarray_dataset_gcm["CH4"][gcm_idx].values,
@@ -256,7 +359,7 @@ class CRCMEmulatorDataset(td.Dataset):  # type: ignore[misc]
         xarray_dataset_crcm.close()
 
         return {
-            "input_first_block": input_first_block,
+            **input_first_block,
             "target": target,
             "year": np.array(current_datetime.year),
             "month": np.array(current_datetime.month),
